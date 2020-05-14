001e6c5725271a107aa59e83e73cdeac89f26d3f
bkmaibach <keith@inputlogic.ca>
2020-05-14 11:25:05 -0700

    Use new form code previously discarded in merge

--- src/apps/chatter/rooms/index.js
+++ src/apps/chatter/rooms/index.js
@@ -1,11 +1,13 @@
 import { useRequest } from '@app-elements/use-request'
 import { Link } from '@app-elements/router'
+import Form, { SubmitButton } from '@app-elements/form'
 import LoadingIndicator from '@app-elements/loading-indicator'
-import url from '/util/url'
-import { useEffect, useState } from 'react'
+
 import { TextInput } from '/elements/text-input'
-import store, { getState } from '/store'
-import { request } from '@app-elements/use-request/request'
+
+import store, { dispatch, clearRequest } from '/store'
+import url from '/util/url'
+
 import './rooms.less'
 
 const RoomItem = ({ id, name }) => (
@@ -15,63 +17,40 @@ const RoomItem = ({ id, name }) => (
 )
 
 export const Rooms = () => {
-  const [newNameInput, setNewNameInput] = useState('')
-  // I am nearly certain there is a smarter way to accomplish this...
-  const [newRoomToRender, setNewRoomToRender] = useState(true)
-  const [roomsAreLoading, setRoomsAreLoading] = useState(false)
-  const [roomList, setRoomList] = useState([])
-  const { token } = getState()
+  const { result, error, isLoading } = useRequest(store, url('api.rooms'))
 
-  useEffect(() => {
-    console.log('USING EFFECT')
-    if (newRoomToRender) {
-      console.log('REQUESTING ROOMS')
-      const { result, error, isLoading } = useRequest(store, url('api.rooms'))
-      setRoomsAreLoading(isLoading)
-      if (error) {
-        console.log(error)
-      }
-      console.log('REQUESTING ROOMS')
-      setRoomList(result.results)
-      // I am nearly certain there is a smarter way to accomplish this...
-      setNewRoomToRender(false)
-    }
-  }, [newRoomToRender])
-
-  const handleCreateNewRoom = () => {
-    const { xhr, promise } = request({
-      url: url('api.rooms'),
-      method: 'post',
-      headers: {
-        Authorization: `token ${token}`
-      },
-      data: {
-        name: newNameInput
-      }
-    })
-    promise.then((res) => {
-      setNewRoomToRender(true)
-      setNewNameInput('')
-      console.log('Response obtained: ', { res })
-    }).catch((err) => { console.warn({ err, xhr }) })
+  if (isLoading) {
+    return <div className='container mt-2'><LoadingIndicator /></div>
   }
 
-  const handleInputChange = (e) => {
-    setNewNameInput(e.target.value)
+  if (error) {
+    return <div>Error!</div>
+  }
+
+  const formProps = {
+    name: 'CreateRoom',
+    action: url('api.rooms'),
+    method: 'post',
+    onSuccess: (res) => {
+      console.log('onSuccess', { res })
+      // useRequest caches results, so we dispatch the clearRequest
+      // action to clear the cache, which will trigger the above `useRequest`
+      // call to re-fetch data from the server.
+      dispatch(clearRequest(url('api.rooms')))
+    },
+    onFailure: (err) => {
+      console.error('onSuccess', { err })
+    }
   }
 
   return (
     <div className='container pt-7 pb-4'>
-      {roomsAreLoading && <div className='container mt-2'><LoadingIndicator /></div>}
-      {roomList.map(({ id, name }) => RoomItem({ id, name }))}
-      <TextInput id='new-room-input'
-        type='text'
-        size='100'
-        value={newNameInput}
-        onChange={handleInputChange} />
-      <button className='btn' onClick={handleCreateNewRoom} disabled={newNameInput === ''} >
-        Create New Room
-      </button>
+      {result.results.map(RoomItem)}
+
+      <Form {...formProps}>
+        <TextInput name='name' placeholder='Room Name' required isFormField />
+        <SubmitButton className='btn'>Create New Room</SubmitButton>
+      </Form>
     </div>
   )
 }
