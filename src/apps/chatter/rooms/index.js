a223cdd6b0e044016251a43a97d3e5f51ed1eeed
bkmaibach <keith@inputlogic.ca>
2020-05-14 09:29:08 -0700

    Change form component to request use

--- src/apps/chatter/rooms/index.js
+++ src/apps/chatter/rooms/index.js
@@ -1,38 +1,48 @@
 // We are going to use ListResource to fetch and display results of
 // an API request. You could also use the lower-level [withRequest HoC](https://github.com/inputlogic/elements/tree/master/components/with-request)
-import Form from '@app-elements/form'
 import { useRequest } from '@app-elements/use-request'
 import LoadingIndicator from '@app-elements/loading-indicator'
-import { Link, routeTo } from '@app-elements/router'
+import { Link } from '@app-elements/router'
 import url from '/util/url'
 import './rooms.less'
-import store from '/store'
+import store, { getState } from '/store'
+import { request } from '@app-elements/use-request/request'
+import React, { useState } from 'react'
 // import React from 'react'
 
-const RoomItem = ({ id, name }) =>
+const RoomItem = ({ id, name }) => (
   <div class='room-item'>
     <h3><Link name='room' args={{ id }}>{name}</Link></h3>
   </div>
+)
 
 export const Rooms = () => {
-  // const [textInput, setTextInput] = useState('')
-  const { result, error, isLoading } = useRequest(store, url('api.rooms'))
+  const [newNameInput, setNewNameInput] = useState('')
+  const { token } = getState()
 
-  const formProps = {
-    name: 'NewRoomForm',
-    action: url('api.rooms'),
-    method: 'post',
-    noAuth: false,
-    initialData: {
-    },
-    onSuccess: (result) => {
-      console.log('onSuccess with result ', { result })
-      routeTo('rooms')
-    },
-    onFailure: (err) => {
-      console.log('onFailure', err)
-    }
+  const handleCreateNewRoom = () => {
+    const { xhr, promise } = request({
+      url: url('api.rooms'),
+      method: 'post',
+      headers: {
+        Authorization: `token ${token}`
+      },
+      data: {
+        name: newNameInput
+      }
+    })
+    promise.then((res) => {
+      setNewNameInput('')
+      console.log('Response obtained: ', { res })
+      // const hasConflict = res.payload.conflicts.length > 0
+    }).catch((err) => { console.warn({ err, xhr }) })
   }
+
+  const handleInputChange = (e) => {
+    setNewNameInput(e.target.value)
+  }
+
+  const { result, error, isLoading } = useRequest(store, url('api.rooms'))
 
   if (isLoading) {
     return <div className='container mt-2'><LoadingIndicator /></div>
@@ -43,11 +53,14 @@ export const Rooms = () => {
   return (
     <div className='container pt-7 pb-4'>
       {result.results.map(({ id, name }) => RoomItem({ id, name }))}
-
-      <Form {...formProps}>
-        <input isFormField name='name' />
-        <button type='submit'>Create New Room</button>
-      </Form>
+      <input id='new-room-input'
+        type='text'
+        size='100'
+        value={newNameInput}
+        onChange={handleInputChange} />
+      <button className='btn' onClick={handleCreateNewRoom} disabled={newNameInput === ''} >
+        Create New Room
+      </button>
     </div>
   )
 }
