d91c658a8b4cfe0b8feb6e8c1ca24cab83a7adb3
bkmaibach <keith@inputlogic.ca>
2020-05-15 11:32:02 -0700

    Remove leftover code in chatbox

--- src/apps/chatter/components/chatbox/chatbox.js
+++ src/apps/chatter/components/chatbox/chatbox.js
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from 'react'
-
 import { Message } from '/elements/message'
 import { useRoom } from '/apps/chatter/hooks/use-room'
 import { MessageInput } from '../message-input'
@@ -7,11 +5,6 @@ import './chatbox.less'
 
 export const ChatBox = ({ roomId }) => {
   const [messages, sendNewMessage] = useRoom(roomId)
-  const inputEl = useRef(null)
-
-  useEffect(() => {
-    inputEl.current && inputEl.current.focus()
-  }, [messages])
 
   const handleSend = (text) => {
     console.log('sending')
